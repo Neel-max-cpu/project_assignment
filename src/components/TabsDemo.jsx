913e6dc0b92f0e006df24efedcf058139c80595c
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-12-22 23:31:50 +0530

    sign in page :memo:

--- src/components/TabsDemo.jsx
+++ src/components/TabsDemo.jsx
@@ -30,29 +30,29 @@ export function TabsDemo() {
                 <TabsList className="grid w-full grid-cols-2">
                     <TabsTrigger className="data-[state=active]:bg-blue-500 data-[state=active]:text-white bg-gray-100 text-gray-700 hover:bg-gray-200" value="op1">SAAS</TabsTrigger>
                     <TabsTrigger className="data-[state=active]:bg-blue-500 data-[state=active]:text-white bg-gray-100 text-gray-700 hover:bg-gray-200" value="op2">Self Hosted</TabsTrigger>
-                </TabsList>
-
+                </TabsList>                
                 {/* op1 for saas */}
                 <TabsContent value="op1">
-                    <Card className="h-[300px] rounded-lg">
-                        <CardContent className="my-10">                    
-                            <div className="flex flex-col items-center space-y-4">
-                                <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
+                    <Card className="h-[300px] rounded-lg shadow-none border-none">                        
+                        <CardContent className="h-full py-6 px-4">
+                            {/* <div className="flex flex-col items-center space-y-4">                                */}
+                            <div className="flex flex-col justify-between h-full">
+                                <Button className="w-full h-10 space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={gitH} alt="GitHub" className="w-5 h-5" />
-                                    <span>Sign in with GitHub</span>
-                                </Button>
+                                    <span className="text-base">Sign in with GitHub</span>
+                                </Button>                               
                                 <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={bitB} alt="GitHub" className="w-5 h-5" />
-                                    <span>Sign in with Bitbucket</span>
-                                </Button>
+                                    <span className="text-base">Sign in with Bitbucket</span>
+                                </Button>                               
                                 <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={azureD} alt="GitHub" className="w-5 h-5" />
-                                    <span>Sign in with Azure Devops</span>
-                                </Button>
+                                    <span className="text-base">Sign in with Azure Devops</span>
+                                </Button>                               
                                 <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={gitL} alt="GitHub" className="w-5 h-5" />
-                                    <span>Sign in with GitLab</span>
-                                </Button>
+                                    <span className="text-base">Sign in with GitLab</span>
+                                </Button>                                
                             </div>
                         </CardContent>
                     </Card>
@@ -60,16 +60,17 @@ export function TabsDemo() {
 
                 {/* op2 for self hosted */}
                 <TabsContent value="op2">
-                    <Card className="h-[300px]">
-                        <CardContent className="my-10">
-                            <div className="flex flex-col items-center space-y-4">
+                    <Card className="h-[300px] rounded-lg shadow-none border-none">
+                        {/* <CardContent className="my-10"> */}
+                        <CardContent className="h-full py-6 px-4">
+                            <div className="flex flex-col items-center space-y-4">                        
                                 <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={gitL} alt="GitHub" className="w-5 h-5" />
-                                    <span>Self Hosted GitLabs</span>
+                                    <span className="text-base">Self Hosted GitLabs</span>
                                 </Button>
                                 <Button className="w-full space-x-2 flex justify-center items-center border border-gray-300 shadow-none rounded-md">
                                     <img src={key} alt="GitHub" className="w-5 h-5" />
-                                    <span>Sign in with SSO</span>
+                                    <span className="text-base">Sign in with SSO</span>
                                 </Button>
                             </div>
                         </CardContent>
